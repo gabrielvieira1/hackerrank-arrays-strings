63738806c036746e98817121f4903fe54a43f7f1
Gabriel Vieira <gabrielvieira1@protonmail.com>
2022-09-03 21:50:28 -0300

    Lendo com gets

--- main.c
+++ main.c
@@ -15,6 +15,12 @@ int main()
 
  // Todos os scanf devem ser passado o ponteiro(Endereço de memória) com & exceto os do tipo string
  // porque um vetor de string já é um ponteiro.
+ /* Riscos de usar o Scanf 
+ 
+ O scanf por padrão só ler até o primeiro espaço e não se preocupa com o tamanho do vetor e pode 
+ acabar lendo mais caracteres do que o tamanho do vetor.
+
+ */
  printf("Digite um numero inteiro\n");
  scanf("%d", &numI); //
 
@@ -22,12 +28,17 @@ int main()
  scanf("%f", &numF);
 
  printf("Digite uma letra\n");
- scanf(" %c", &letra);
+ //scanf("%c", &letra); // Buffer do teclado ->45\n12.40\n (Letra está sendo preenchida pelo espaço em branco
+ scanf(" %c", &letra); // Corrigimos adicionando um espaço em branco na frente da mascara. Desonsidera enter, espaço ou tab.
 
  printf("Digite duas palavras\n");
  // scanf("%s", palavras); //Vai ler até encontrar o primeiro espaço
  // scanf(" %[^\n]", palavras); //Vai dar o erro no compilador * stack smashing detected * vulnerabilidade severa
- scanf(" %10[^\n]", palavras); // Vai ler até o tamanho determinado e com espaços em branco
+ //scanf(" %10[^\n]", palavras); // Vai ler até o tamanho determinado e com espaços em branco (Segura)
+
+ // Lendo com get
+ //gets(palavras); // O uso de gets pode levar a graves vulnerabilidades de buffer overflow. Use a alternativa segura fgets.
+ fgets(palavras, 10, stdin); // só vai ler o tamanho - 1 (Segura)
 
  printf("%d\n", numI);
  printf("%f\n", numF);
